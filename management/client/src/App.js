159fb64a728db2bae7677db33d7c18c3e351b51b
Chang-Young Park <parkcy0630@gmail.com>
2024-06-13 23:20:14 +0900

    Lesson 9 - Add Customer API Loading View (Progress Bar)

--- management/client/src/App.js
+++ management/client/src/App.js
@@ -8,8 +8,9 @@ import {
   TableBody,
   TableRow,
   TableCell,
+  CircularProgress,
 } from "@mui/material";
-import { styled } from "@mui/system";
+import { styled, useTheme } from "@mui/system";
 
 // Styled 컴포넌트 정의
 const StyledPaper = styled(Paper)({
@@ -22,8 +23,14 @@ const StyledTable = styled(Table)({
   minWidth: 1080,
 });
 
+const StyledCircularProgress = styled(CircularProgress)(({ theme }) => ({
+  margin: theme.spacing(2),
+}));
+
 function App() {
   const [customers, setCustomers] = useState("");
+  const [completed, setCompleted] = useState(0);
+  const theme = useTheme();
 
   useEffect(() => {
     const callApi = async () => {
@@ -32,9 +39,20 @@ function App() {
       return body;
     };
 
-    callApi()
-      .then((res) => setCustomers(res))
-      .catch((err) => console.log(err));
+    // callApi()
+    //   .then((res) => setCustomers(res))
+    //   .catch((err) => console.log(err));
+
+    const progress = () => {
+      setCompleted((prevCompleted) =>
+        prevCompleted >= 100 ? 0 : prevCompleted + 1
+      );
+    };
+
+    const timer = setInterval(progress, 20);
+    return () => {
+      clearInterval(timer);
+    };
   }, []);
 
   return (
@@ -51,19 +69,28 @@ function App() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {customers
-            ? customers.map((c) => (
-                <Customer
-                  key={c.id} // map 을 사용하려면 key 라는 속성이 있어야 함(안하면 Console창에 에러가 발생)
-                  id={c.id}
-                  image={c.image}
-                  name={c.name}
-                  birthday={c.birthday}
-                  gender={c.gender}
-                  job={c.job}
+          {customers ? (
+            customers.map((c) => (
+              <Customer
+                key={c.id} // map 을 사용하려면 key 라는 속성이 있어야 함(안하면 Console창에 에러가 발생)
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan="6" align="center">
+                <StyledCircularProgress
+                  variant="indeterminate"
+                  value={completed}
                 />
-              ))
-            : ""}
+              </TableCell>
+            </TableRow>
+          )}
         </TableBody>
       </StyledTable>
     </StyledPaper>
